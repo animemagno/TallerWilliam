d7d85efcc20bb7528d3e7330b8e453b6fd5a5f1a
animemagno <animemagno0883@gmail.com>
2025-12-02 19:17:39 -0600

    Fix: Ventas HTML - Corregido z-index de botones de grupo, actualización en tiempo real de totales y función de abono a grupos

--- src/config/firebase.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyDh074AarXaYCc-Htw-lsCeIc_95QQNSnY",
-    authDomain: "tallerwilliam-732b3.firebaseapp.com",
-    projectId: "tallerwilliam-732b3",
-    storageBucket: "tallerwilliam-732b3.firebasestorage.app",
-    messagingSenderId: "822262666247",
-    appId: "1:822262666247:web:6680487bbf1108006b86a2"
-};
